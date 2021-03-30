a18f5c47f04a8133fb29bc0a6ab734a5ba27bd52
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-03-30 19:45:12 +0200

    Prevented the intro music from playing at every restart

--- Balance Prototype/Assets/Scripts/DontDestroyOnLoad.cs
+++ Balance Prototype/Assets/Scripts/DontDestroyOnLoad.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    private static bool isSpawned = false;
     // Start is called before the first frame update
     private void Awake()
     {
+        if (!isSpawned)
+        {
+            isSpawned = true;
         DontDestroyOnLoad(this.gameObject);
-    }
+        }
+        else
+        {
+            DestroyImmediate(this.gameObject);
+        }
+}
 }

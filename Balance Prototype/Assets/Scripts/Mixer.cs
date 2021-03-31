3eb742e9c03c7f2dabc353d9a608363d945554bb
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-03-31 21:29:17 +0200

    Added the level music to a mixer group

---
+++ Balance Prototype/Assets/Scripts/Mixer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Mixer : MonoBehaviour
+{
+    public AudioMixer mixer;
+    
+    
+    public void FadeVolume()
+      {
+        StartCoroutine(FadeMixerGroup.StartFade( mixer, "levelMusic", 0.2f, 0.5f));
+    }
+}

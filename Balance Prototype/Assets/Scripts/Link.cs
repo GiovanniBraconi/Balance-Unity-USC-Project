ed10b9894985d6cc9689ab0cdbc382349b417c1f
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-06-03 14:30:16 +0200

    Added an about panel with a link to the itch.io page

---
+++ Balance Prototype/Assets/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenItchPage()
+	{
+#if !UNITY_EDITOR
+		openWindow("https://ammazzadrago.itch.io/");
+#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
